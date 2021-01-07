e2ac0b0ce450c20bc271b4d1c87a9407dcedc733
Justin Stauffer <justindstauffer@gmail.com>
2021-01-07 14:18:48 -0500

    main

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
+import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [test, setTest] = useState()
+  const getTest = () => {
+    axios.get("http://localhost:5000")
+      .then((res) => {
+        setTest(res.data)
+      })
+  }
+  useEffect(() => {
+    getTest()
+  })
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {test}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
